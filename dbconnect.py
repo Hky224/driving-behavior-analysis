2f4189d818bcfce09fe5cbf63830c450fe8d6a61
Hky224 <22018063d@connect.polyu.hk>
2024-04-16 22:48:57 +0800

    Add files via upload

---
+++ dbconnect.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+
+
+def connect():
+    mydb = mysql.connector.connect( host = 'comp4442-gp.crqk44cigx2u.us-east-1.rds.amazonaws.com',
+	user = 'admin',
+	port = '3306',
+	database = 'comp4442-gp',
+	passwd = '12345678'
+    )
+    return mydb
+
